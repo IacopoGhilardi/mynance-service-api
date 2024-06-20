c86b8a4a7c0e9164298d2c93716804d116298995
Iacopo Ghilardi <iacopo.ghilardi@a-tono.com>
2024-06-20 14:06:18 +0200

    Updated makefile

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,5 @@
-#docker command
-docker run --name mydget-db -e POSTGRES_PASSWORD=mypassword -e POSTGRES_USER=mydget -e POSTGRES_DB=mydget -p 5432:5432 -d postgres:latest
+#DOCKER
+docker compose up --build
 
 
 #MIGRATIONS
